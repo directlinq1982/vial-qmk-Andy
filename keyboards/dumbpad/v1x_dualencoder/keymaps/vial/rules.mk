5e929565dfdd298b19a9f500d90be267a95e13b5
directlinq1982 <directlinq@gmail.com>
2026-05-13 19:03:24 +0100

    Update rules.mk

--- keyboards/dumbpad/v1x_dualencoder/keymaps/vial/rules.mk
+++ keyboards/dumbpad/v1x_dualencoder/keymaps/vial/rules.mk
@@ -2,16 +2,12 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = no
-
-# The biggest QMK space saver
 LTO_ENABLE = yes
 
-# --- THE FIRMWARE DIET ---
-# Explicitly turning off base Dumbpad defaults
+# The Final Firmware Diet
+BOOTMAGIC_ENABLE = no
 MOUSEKEY_ENABLE = no
 EXTRAKEY_ENABLE = no
-
-# Aggressive space savers
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 SPACE_CADET_ENABLE = no
@@ -21,6 +17,4 @@ MUSIC_ENABLE = no
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = no
-
-# The biggest Vial space saver (saves ~1.5KB)
 VIAL_QMK_SETTINGS = no
