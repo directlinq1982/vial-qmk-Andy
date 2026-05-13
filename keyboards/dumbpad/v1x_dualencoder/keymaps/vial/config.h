72424d3cc5b52b3f1691ebde9ec2d30d161f8fd1
directlinq1982 <directlinq@gmail.com>
2026-05-13 19:04:13 +0100

    Update config.h

--- keyboards/dumbpad/v1x_dualencoder/keymaps/vial/config.h
+++ keyboards/dumbpad/v1x_dualencoder/keymaps/vial/config.h
@@ -1,21 +1,25 @@
 #pragma once
-#define VIAL_KEYBOARD_UID {0x49, 0x6B, 0x7A, 0xA8, 0x6C, 0x78, 0xF6, 0x21}
 
-#undef DYNAMIC_KEYMAP_LAYER_COUNT
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
-
-#define VIAL_TAP_DANCE_ENTRIES 10
-#define VIAL_COMBO_ENTRIES 10
-
-#define DYNAMIC_KEYMAP_MACRO_COUNT 20
-/* 500 bytes allows for exactly 500 characters total across all 20 macros */
-#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 500 
-
+// Vial & Memory Settings
+#define VIAL_KEYBOARD_UID {0x8A, 0xE4, 0xC2, 0x19, 0xF3, 0x7B, 0xD5, 0x41}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
+#undef DYNAMIC_KEYMAP_LAYER_COUNT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define VIAL_TAP_DANCE_ENTRIES 10
+#define VIAL_COMBO_ENTRIES 10
+#define DYNAMIC_KEYMAP_MACRO_COUNT 20
+#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 500 
+
+// Encoder Settings
 #undef ENCODER_A_PINS
 #undef ENCODER_B_PINS
 #define ENCODER_A_PINS { F5, D4 } 
-#define ENCODER_B_PINS { F4, D0 }
+#define ENCODER_B_PINS { F4, D3 }
 #define ENCODER_RESOLUTION 4
+
+// Extreme Space Savers
+#define NO_PRINT
+#define NO_DEBUG
+#define NO_ACTION_ONESHOT
