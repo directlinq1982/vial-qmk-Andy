ac7655263c43734e1e4475bb8bdb4c9a09a3fcd4
directlinq1982 <directlinq@gmail.com>
2026-05-13 18:02:35 +0100

    Update config.h

--- keyboards/dumbpad/v1x_dualencoder/keymaps/vial/config.h
+++ keyboards/dumbpad/v1x_dualencoder/keymaps/vial/config.h
@@ -1,13 +1,13 @@
 #pragma once
 #define VIAL_KEYBOARD_UID {0x49, 0x6B, 0x7A, 0xA8, 0x6C, 0x78, 0xF6, 0x21}
 
-#pragma once
-
 #undef DYNAMIC_KEYMAP_LAYER_COUNT
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
 #define VIAL_TAP_DANCE_ENTRIES 10
 #define VIAL_COMBO_ENTRIES 10
 
-#define DYNAMIC_KEYMAP_MACRO_COUNT 30
-#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 400 
+#define DYNAMIC_KEYMAP_MACRO_COUNT 20
+/* 500 bytes allows for exactly 500 characters total across all 20 macros */
+#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 500 
+
