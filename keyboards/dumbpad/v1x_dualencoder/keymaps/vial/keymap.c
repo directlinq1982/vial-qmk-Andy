1cdac8425de9a6d2f5f20babd34acfe88b604536
directlinq1982 <directlinq@gmail.com>
2026-05-13 18:48:22 +0100

    Update keymap.c

--- keyboards/dumbpad/v1x_dualencoder/keymaps/vial/keymap.c
+++ keyboards/dumbpad/v1x_dualencoder/keymaps/vial/keymap.c
@@ -67,88 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // If console is enabled, it will print the matrix position and status of each key pressed
-/*
-#ifdef CONSOLE_ENABLE
-    uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
-#endif
-*/
     return true;
 }
 
 void keyboard_post_init_user(void) {
-    // Customise these values to desired behaviour
-    // debug_enable = true;
-    // debug_matrix = true;
-    // debug_keyboard = true;
-    // debug_mouse = true;
 }
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    /*  Custom encoder control - handles CW/CCW turning of encoder
-     *  Default behavior:
-     *    left encoder:
-     *      main layer:
-     *         CW: move mouse right
-     *        CCW: move mouse left
-     *      other layers:
-     *         CW: = (equals/plus - increase slider in Adobe products)
-     *        CCW: - (minus/underscore - decrease slider in adobe products)
-     *    right encoder:
-     *      main layer:
-     *         CW: colume up
-     *        CCW: volume down
-     *      other layers:
-     *         CW: right arrow
-     *        CCW: left arrow
-     */
-    if (index == 0) {
-        switch (get_highest_layer(layer_state)) {
-            case 0:
-                // main layer - move mouse right (CW) and left (CCW)
-                if (clockwise) {
-                    tap_code(KC_MS_R);
-                } else {
-                    tap_code(KC_MS_L);
-                }
-                break;
-
-            default:
-                // other layers - =/+ (quals/plus) (CW) and -/_ (minus/underscore) (CCW)
-                if (clockwise) {
-                    tap_code(KC_EQL);
-                } else {
-                    tap_code(KC_MINS);
-                }
-                break;
-        }
-    } else if (index == 1) {
-        switch (get_highest_layer(layer_state)) {
-            case 0:
-                // main layer - volume up (CW) and down (CCW)
-                if (clockwise) {
-                    tap_code(KC_VOLU);
-                } else {
-                    tap_code(KC_VOLD);
-                }
-                break;
-
-            default:
-                // other layers - right (CW) and left (CCW)
-                if (clockwise) {
-                    tap_code(KC_RIGHT);
-                } else {
-                    tap_code(KC_LEFT);
-                }
-                break;
-        }
-    }
-    return true;
-}
-
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = { ENCODER_CCW_CW(KC_A, KC_B), ENCODER_CCW_CW(KC_C, KC_D) },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [2] = { ENCODER_CCW_CW(KC_A, KC_B), ENCODER_CCW_CW(KC_C, KC_D) },
-    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-};
