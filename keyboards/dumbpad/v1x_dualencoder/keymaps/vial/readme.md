d39c7c4f4d44c0c7fdc7c770aa8f84f58671295a
directlinq1982 <directlinq@gmail.com>
2026-05-14 13:51:01 +0100

    Add files via upload

---
+++ keyboards/dumbpad/v1x_dualencoder/keymaps/vial/readme.md
@@ -0,0 +1,4 @@
+## Dumbpad Vial Source v1.01
+- These are the `/keymap` files neccesary to compile your own Vial build of the firmware.
+- You will require both the QMK and Vial build environments set up on your system.
+- You will also have to modify the root config.h to match the vendor and product ID of the files here to compile correctly.
--- keyboards/dumbpad/v1x_dualencoder/keymaps/vial/rules.mk
+++ keyboards/dumbpad/v1x_dualencoder/keymaps/vial/rules.mk
@@ -1,21 +1,32 @@
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = no
-LTO_ENABLE = yes
-VIAL_ENCODERS_ENABLE = yes
+# MCU name
+MCU = atmega32u4
 
-# The Final Firmware Diet
-BOOTMAGIC_ENABLE = no
-MOUSEKEY_ENABLE = no
-EXTRAKEY_ENABLE = no
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE = no
-MAGIC_ENABLE = no
-MUSIC_ENABLE = no
-AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = no
-VIAL_QMK_SETTINGS = no
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+VIA_ENABLE = yes						# Enable VIA compatibility
+VIAL_ENABLE = yes						# Enable Vial compatibility
+QMK_SETTINGS = no						# Enable Vial QMK Settings Menu
+TAP_DANCE_ENABLE = no       # Enable Vial Tap Dance Menu
+COMBO_ENABLE = no           # Enable Vial Combo Menu
+LTO_ENABLE = yes						# Enable Link Time Optimization
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
+
+ENCODER_ENABLE = yes
+VIAL_ENCODERS_ENABLE = yes  # Enable Vial Rotary Encoders
+KEY_LOCK_ENABLE = yes
